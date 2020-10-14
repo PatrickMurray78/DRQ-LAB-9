117b15d55ca7a5cb405edc73f3493c2c5b637a7e
patrickmurray73 <g00344530@gmit.ie>
2020-10-14 12:14:06 +0100

    Completed Exercise 6

--- src/App.js
+++ src/App.js
@@ -6,22 +6,29 @@ import { Content } from './components/content';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav } from 'react-bootstrap';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar bg="primary" variant="dark">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/read">Read</Nav.Link>
-            <Nav.Link href="/create">Create</Nav.Link>
-          </Nav>
-        </Navbar>
-        <Header></Header>
-        <Content></Content>
-        <Footer></Footer>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar bg="primary" variant="dark">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Navbar>
+
+          <br/>
+          <Switch>
+            <Route path='/' component={Content} exact/>
+            <Route path='/read' component={Header} exact/>
+            <Route path='/create' component={Footer} exact/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
