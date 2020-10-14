a63ab6ab6ea8e03560cbba6f634bde23f19ffe26
patrickmurray73 <g00344530@gmit.ie>
2020-10-14 12:08:38 +0100

    Completed Exercise 5

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css';
 import { Header } from './components/header';
 import { Footer } from './components/footer';
 import { Content } from './components/content';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <div className="App">
+        <Navbar bg="primary" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/read">Read</Nav.Link>
+            <Nav.Link href="/create">Create</Nav.Link>
+          </Nav>
+        </Navbar>
         <Header></Header>
         <Content></Content>
         <Footer></Footer>
