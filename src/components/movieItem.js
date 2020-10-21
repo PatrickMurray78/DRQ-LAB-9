f874819d824c3a69f8d20ebf01007a6e2442c73a
patrickmurray73 <g00344530@gmit.ie>
2020-10-21 12:21:49 +0100

    Add card to movieItem

--- src/components/movieItem.js
+++ src/components/movieItem.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 export class MovieItem extends React.Component {
 
     render() {
         return(
             <div>
-                <h3>{this.props.mymovie.Title}</h3>
+                {/*<h3>{this.props.mymovie.Title}</h3>
                 <p>{this.props.mymovie.Year}</p>
-                <img src={this.props.mymovie.Poster} width="200" height="200"></img>
-            </div>
+        <img src={this.props.mymovie.Poster} width="200" height="200"></img>*/}
+        
+            <Card>
+                <Card.Header>{this.props.mymovie.Title}</Card.Header>
+                <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                        <img src={this.props.mymovie.Poster}></img>
+                        <footer className="blockquote-footer">
+                            {this.props.mymovie.Year}
+                        </footer>
+                    </blockquote>
+                </Card.Body>
+            </Card>
+        </div>
         );
     }
 }
--- src/components/read.js
+++ src/components/read.js
@@ -33,7 +33,7 @@ export class Read extends React.Component {
     render(){
         return (
           <div>
-            <h3>Hello from Read</h3>
+            {/*<h3>Hello from Read</h3>*/}
             <Movies mymovies={this.state.movies}></Movies>
           </div>
         );
