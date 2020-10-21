2055ad97e8e3facbd15add4f794990d75f4b69e1
patrickmurray73 <g00344530@gmit.ie>
2020-10-21 12:50:08 +0100

    Added comments

--- src/components/content.js
+++ src/components/content.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+// The Content class is the default page that is displayed, when the website is ran
 export class Content extends React.Component {
 
     render(){
         return (
-          <div>
+          <div> {/*This page simply says hello world! and also displays the current time*/}
             <h1>Hello World!</h1>
             <h2>It is {new Date().toLocaleTimeString()} PM.</h2>
           </div>
