be83e2a2dda5e10262b2459e603dd068698931ed
patrickmurray73 <g00344530@gmit.ie>
2020-10-27 10:48:51 +0000

    Create lifecycle hook to get JSON data

--- src/components/read.js
+++ src/components/read.js
@@ -4,21 +4,27 @@ import axios from 'axios';
 
 // The Read class reads in the stored movies and sends them to the movies component
 export class Read extends React.Component {
-    // state contains an array 'movies' which contains all the stored movies to be displayed
-    state = {
-        movies: [] 
-    };
+  // state contains an array 'movies' which contains all the stored movies to be displayed
+  state = {
+    movies: []
+  };
 
-    componentDidMount(){
+  componentDidMount() {
+    axios.get('https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')
+      .then((response) => {
+        this.setState({ movies: response.data.Search })
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+  }
 
-    }
-
-    // This function creates a new array 'mymovies' and sends it to the movies component
-    render(){
-        return (
-          <div>
-            <Movies mymovies={this.state.movies}></Movies>
-          </div>
-        );
-    }
+  // This function creates a new array 'mymovies' and sends it to the movies component
+  render() {
+    return (
+      <div>
+        <Movies mymovies={this.state.movies}></Movies>
+      </div>
+    );
+  }
 }
