00eb84e130085b8f967cc578b81cfa8533079e29
patrickmurray73 <g00344530@gmit.ie>
2020-10-28 10:50:42 +0000

    Added comments

--- src/components/create.js
+++ src/components/create.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 // The Create class will be used to create new movie entries
 export class Create extends React.Component {
-
   constructor() {
     super();
 
+    // Bind new data to corresponding variables
     this.onSubmit = this.onSubmit.bind(this);
     this.onChangeTitle = this.onChangeTitle.bind(this);
     this.onChangeYear = this.onChangeYear.bind(this);
@@ -18,24 +18,28 @@ export class Create extends React.Component {
     }
   }
 
+  // When title is changed, set the states title to the new one
   onChangeTitle(e) {
     this.setState({
       Title: e.target.value
     });
   }
 
+  // When Year is changed, set the states year to the new one
   onChangeYear(e) {
     this.setState({
       Year: e.target.value
     });
   }
 
+  // When Poster is changed, set the states poster to the new one
   onChangePoster(e) {
     this.setState({
       Poster: e.target.value
     });
   }
 
+  // When form is submitted, alert the user with the new movie details
   onSubmit(e) {
     e.preventDefault();
     alert("Movie: " + this.state.Title + " "
@@ -43,6 +47,10 @@ export class Create extends React.Component {
       this.state.Poster);
   }
 
+  // Inside this render() function we create a form. This form has
+  // three input boxes for the title, year and poster. There is also
+  // a submit button at the bottom. Once clicked the information is output
+  // to screen.
   render() {
     return (
       <div className='App'>
--- src/components/read.js
+++ src/components/read.js
@@ -4,11 +4,13 @@ import axios from 'axios';
 
 // The Read class reads in the stored movies and sends them to the movies component
 export class Read extends React.Component {
-  // state contains an array 'movies' which contains all the stored movies to be displayed
+  // state contains an ampty array 'movies' which will contain all the stored movies to be displayed
   state = {
     movies: []
   };
 
+  // This function uses axios which is a promise based HTTP client
+  // We then use it to create a lifecycle hook that returns the JSON data
   componentDidMount() {
     axios.get('https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')
       .then((response) => {
