49f05d4d65f1435ae2252ba9549a29fe226fb53d
patrickmurray73 <g00344530@gmit.ie>
2020-11-11 13:16:03 +0000

    Create BackEnd folder

---
+++ BackEnd/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/api/movies', (req, res) => {
+   
+   const mymovies = [
+        {
+            "Title":"Avengers: Infinity War",
+            "Year":"2018",
+            "imdbID":"tt4154756",
+            "Type":"movie",
+            "Poster":"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+        },
+        {
+            "Title":"Captain America: Civil War",
+            "Year":"2016",
+            "imdbID":"tt3498820",
+            "Type":"movie",
+            "Poster":"https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+        }
+   ];
+    res.status(200).json({
+        message:"Everything is ok",
+        movies:mymovies});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
